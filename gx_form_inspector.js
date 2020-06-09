2ff69fd2971dc7a9a1f5245606c17aa989aef137
Marcos Crispino <mcrispino@genexus.com>
2020-06-09 11:27:32 -0300

    Added initial version of gx_form_inspector.js

---
+++ gx_form_inspector.js
@@ -0,0 +1,80 @@
+//gx.forminspector(ctrl_gxid, row, gxobjectWC)
+//ctrl_gxid is the control name as defined in Genexus
+//row is a optional filter in case that then selected control is in a grid level
+//gxobjectWC is an optional filter to do the search over a specific Genexus WebComponent
+//Return: array of objects with each instance of the control with the specified GXId in the HTML form
+
+//Each element has the form 
+//"inMasterPage":boolean
+//"cmp":string(id) if the controls is not in a component this value will be empty
+//"id":string(controlDOMId)
+//"value":string(controlvalue)
+
+window.addEventListener("load", () => {
+	
+	gx.forminspector = function (ctrl_gxid, row, gxobjectWC) {
+		const gx_control_att = 'data-gx-control-name';
+		const initailize_gx_object = function(gxo) {
+				gxo.GXCtrlIds.map( i => {
+						let vStruct = gxo.GXValidFnc[i];
+						if (vStruct){
+							let gxName = '';
+							if (vStruct.fld) {
+								if (vStruct.gxvar) {
+									//Genexus Var or Att
+									let bIsVar = vStruct.gxvar.startsWith('AV');
+									gxName = `${(bIsVar?'&':'')}${vStruct.gxvar.replace(/^AV?\d*/, '')}` ;
+								}
+								else {
+									gxName = vStruct.fld;
+								}
+								let rExp = new RegExp(`(span_)?${gxo.CmpContext}${vStruct.fld}(_([0-9]{4})*)?`);
+								$('[id]').filter(function() {return $(this)[0].id.match(rExp)}).attr(gx_control_att, gxName.toLowerCase());
+							}
+						}
+					});
+		};
+		const _init = () => {
+			if (!gx.forminspector._initialized) {
+				initailize_gx_object( gx.O);
+				initailize_gx_object( gx.pO.MasterPage);
+				gx.O.WebComponents.map( gxComponent => initailize_gx_object( gxComponent));
+				gx.fx.obs.addObserver('webcom.render', gx.forminspector, gxComponent => initailize_gx_object( gxComponent));
+				gx.forminspector._initialized = true;
+			}
+		};
+
+		_init();
+			
+		let srow = (row)?gx.text.padl(row.toString(), 4, '0'):undefined;
+		let selector = `[${gx_control_att}="${ctrl_gxid.toLowerCase()}"]`;			
+		if (row) {
+			selector = `[data-gxrow="${srow}"] ` + selector;
+		}
+		let targets = gx.$(selector);
+		let filterPrefixWC = gxobjectWC ? gx.O.WebComponents.filter((wc) => wc.ServerClass === gxobjectWC.toLowerCase()) : [];
+		
+		ret = gx.$.map( targets, function( target) {
+			let cmpElement = gx.$(target).closest('[class=gxwebcomponent]').map( (i,el) => {
+					let stripCmpName = id => id.replace(/^gxHTMLWrp/,'');
+					let id = el.id;
+					if (filterPrefixWC.length > 0) {
+						let match = filterPrefixWC.filter((wc) => id.endsWith(wc.CmpContext));
+						if (match.length > 0) {
+							return stripCmpName(match[0].CmpContext);
+						}
+					}
+					else {
+						return stripCmpName(id);
+					}
+				}
+			).get().join();
+			let inMasterPage = target.id.endsWith('_MPAGE');
+			if (!gxobjectWC || cmpElement.length > 0) {
+				return [{inMasterPage: inMasterPage, cmp: cmpElement || "", id:target.id , value:target.value || target.textContent}];
+			}
+		});
+		return ret;
+	};
+
+})
