64d094e262d24e87dbedbd7d93fdab9770b8775d
dmendez <dmendez@genexus.com>
2020-10-22 15:42:00 -0300

    add flat polyfill for IE

--- src/gx_form_inspector.js
+++ src/gx_form_inspector.js
@@ -10,6 +10,25 @@
 //"id":string(controlDOMId)
 //"value":string(controlvalue)
 
+// IE polyfill 
+if (!Array.prototype.flat) {
+	Array.prototype.flat = function(depth) {
+	  var flattend = [];
+	  (function flat(array, depth) {
+		for (let el of array) {
+		  if (Array.isArray(el) && depth > 0) {
+			flat(el, depth - 1); 
+		  } else {
+			flattend.push(el);
+		  }
+		}
+	  })(this, Math.floor(depth) || 1);
+	  return flattend;
+	};
+  }
+
+  ///////
+
 window.addEventListener("load", () => {
 	
 	gx.forminspector = function (ctrl_gxid, row, gxobjectWC) {
