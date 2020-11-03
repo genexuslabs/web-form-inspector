ad8083718e44758bb4d06fc20ec78bf02b02e92f
Germán Odulio <german.gonzalez@abstracta.com.uy>
2020-11-03 16:02:21 -0300

    Added event listener again

--- src/gx_form_inspector.js
+++ src/gx_form_inspector.js
@@ -46,109 +46,110 @@ if (!String.prototype.startsWith) {
 }
 
 ///////
-
-gx.forminspector = function (ctrl_gxid, row, gxobjectWC) {
-    const gx_control_att = 'data-gx-control-name';
-    const initialize_gx_object = function(gxo) {
-            (gxo &&
-                gxo.GXCtrlIds &&
-                gxo.GXCtrlIds.map( i => {
-                    let vStruct = gxo.GXValidFnc[i];
-                    if (vStruct){
-                        let gxName = '';
-                        if (vStruct.fld) {
-                            if (vStruct.gxvar) {
-                                //Genexus Var or Att
-                                let bIsVar = vStruct.gxvar.startsWith('AV');
-                                gxName = `${(bIsVar?'&':'')}${vStruct.gxvar.replace(/^AV?\d*/, '')}` ;
+window.addEventListener("load", () => {
+    gx.forminspector = function (ctrl_gxid, row, gxobjectWC) {
+        const gx_control_att = 'data-gx-control-name';
+        const initialize_gx_object = function(gxo) {
+                (gxo &&
+                    gxo.GXCtrlIds &&
+                    gxo.GXCtrlIds.map( i => {
+                        let vStruct = gxo.GXValidFnc[i];
+                        if (vStruct){
+                            let gxName = '';
+                            if (vStruct.fld) {
+                                if (vStruct.gxvar) {
+                                    //Genexus Var or Att
+                                    let bIsVar = vStruct.gxvar.startsWith('AV');
+                                    gxName = `${(bIsVar?'&':'')}${vStruct.gxvar.replace(/^AV?\d*/, '')}` ;
+                                }
+                                else {
+                                    gxName = vStruct.fld;
+                                }
+                                let rExp = new RegExp(`(span_)?${gxo.CmpContext}${vStruct.fld}(_([0-9]{4})*)?$`);
+                                $('[id]').filter(function() {return $(this)[0].id.match(rExp)}).attr(gx_control_att, gxName.toLowerCase());
                             }
-                            else {
-                                gxName = vStruct.fld;
-                            }
-                            let rExp = new RegExp(`(span_)?${gxo.CmpContext}${vStruct.fld}(_([0-9]{4})*)?$`);
-                            $('[id]').filter(function() {return $(this)[0].id.match(rExp)}).attr(gx_control_att, gxName.toLowerCase());
                         }
-                    }
-                })
-            );
-    };
-    const _init = () => {
-        if (!gx.forminspector._initialized) {
-            initialize_gx_object( gx.O);
-            initialize_gx_object( gx.pO.MasterPage);
-            gx.O.WebComponents.map( gxComponent => initialize_gx_object( gxComponent));
-            gx.fx.obs.addObserver('webcom.render', gx.forminspector, gxComponent => initialize_gx_object( gxComponent));
-            gx.forminspector._initialized = true;
-        }
-    };
-
-    _init();
-
-    let srow = (row)?gx.text.padl(row.toString(), 4, '0'):undefined;
-    let selector = `[${gx_control_att}="${ctrl_gxid.toLowerCase()}"]`;
-    if (row) {
-        selector = `[data-gxrow="${srow}"] ` + selector;
-    }
-    let targets = gx.$(selector);
-    const filterPrefixWC = gxobjectWC ? gx.O.WebComponents.filter((wc) => wc.ServerClass === gxobjectWC.toLowerCase()) : [];
-
-    ret = gx.$.map( targets, function( target) {
-        let cmpElement = gx.$(target).closest('[class=gxwebcomponent]').map( (i,el) => {
-                let stripCmpName = id => id.replace(/^gxHTMLWrp/,'');
-                let id = el.id;
-                if (filterPrefixWC.length > 0) {
-                    let match = filterPrefixWC.filter((wc) => id.endsWith(wc.CmpContext));
-                    if (match.length > 0) {
-                        return stripCmpName(match[0].CmpContext);
-                    }
-                }
-                else {
-                    return stripCmpName(id);
-                }
-            }
-        ).get().join();
-        let inMasterPage = target.id.endsWith('_MPAGE');
-        if (!gxobjectWC || cmpElement.length > 0) {
-            return [{inMasterPage: inMasterPage, cmp: cmpElement || "", id:target.id , value:target.value || target.textContent}];
-        }
-    });
-    return ret;
-}
-
-gx.inspector = {
-    messages: function() {
-        //Return array of GX messages (msg) in form
-        return $.map($(".gx_ev"), (n,i) => { const $n = $(n); return ($n.text().length > 0) ? $n.text() : null})
-    },
-    elements: function( opts) {
-        //opts is an object with properties:
-        //ctrl_gxid is the control name as defined in Genexus (Required)
-        //row is a optional filter in case that then selected control is in a grid level (Optional)
-        //gxobjectWC is an optional filter to do the search over a specific Genexus WebComponent (Optional)
-        const {ctrl_gxid = "", row = "", gxobjectWC = ""} = opts;
-        return gx.forminspector(ctrl_gxid, row, gxobjectWC).concat(gx.inspector.grids(opts));
-    },
-    grids: function( opts) {
-        //opts is an object with properties:
-        //ctrl_gxid is the control name as defined in Genexus (Required)
-        //row is a optional filter in case that then selected control is in a grid level (Optional)
-        //Row number shold be of length 4 with '0'left padding
-        //0001001 row1 at parent row1 - 00010002 row2 at parent row1
-        //gxobjectWC is an optional filter to do the search over a specific Genexus WebComponent (Optional)
-        const {ctrl_gxid = "", row = "", gxobjectWC = ""} = opts;
-        const wcFilter = wc => !gxobjectWC || wc.ServerClass === gxobjectWC.toLowerCase() ? wc : null;
-        const gFilter = g => ((g.realGridName.toLowerCase() === ctrl_gxid.toLowerCase()) && (!row || (g.parentRow.gxId === row))) ? g : null;
-        const wcGrids = gx.O.WebComponents.filter(wcFilter).map( wc => wc.Grids).flat();
-        const mpGrids = gx.O.MasterPage ? gx.O.MasterPage.Grids : [];
-        let Grids = (!gxobjectWC ? gx.O.Grids : []).concat(wcGrids).concat(mpGrids);
-        const retObj = g =>	{
-            return {
-                id: g.getContainerControl().id ,
-                cmp: g.parentObject?.CmpContext || "",
-                rows: g.grid.rows.length,
-                inMasterPage: g.parentObject?.IsMasterPage || false,
+                    })
+                );
+        };
+        const _init = () => {
+            if (!gx.forminspector._initialized) {
+                initialize_gx_object( gx.O);
+                initialize_gx_object( gx.pO.MasterPage);
+                gx.O.WebComponents.map( gxComponent => initialize_gx_object( gxComponent));
+                gx.fx.obs.addObserver('webcom.render', gx.forminspector, gxComponent => initialize_gx_object( gxComponent));
+                gx.forminspector._initialized = true;
             }
         };
-        return Grids.filter( gFilter).map(retObj);
+
+        _init();
+
+        let srow = (row)?gx.text.padl(row.toString(), 4, '0'):undefined;
+        let selector = `[${gx_control_att}="${ctrl_gxid.toLowerCase()}"]`;
+        if (row) {
+            selector = `[data-gxrow="${srow}"] ` + selector;
+        }
+        let targets = gx.$(selector);
+        const filterPrefixWC = gxobjectWC ? gx.O.WebComponents.filter((wc) => wc.ServerClass === gxobjectWC.toLowerCase()) : [];
+
+        ret = gx.$.map( targets, function( target) {
+            let cmpElement = gx.$(target).closest('[class=gxwebcomponent]').map( (i,el) => {
+                    let stripCmpName = id => id.replace(/^gxHTMLWrp/,'');
+                    let id = el.id;
+                    if (filterPrefixWC.length > 0) {
+                        let match = filterPrefixWC.filter((wc) => id.endsWith(wc.CmpContext));
+                        if (match.length > 0) {
+                            return stripCmpName(match[0].CmpContext);
+                        }
+                    }
+                    else {
+                        return stripCmpName(id);
+                    }
+                }
+            ).get().join();
+            let inMasterPage = target.id.endsWith('_MPAGE');
+            if (!gxobjectWC || cmpElement.length > 0) {
+                return [{inMasterPage: inMasterPage, cmp: cmpElement || "", id:target.id , value:target.value || target.textContent}];
+            }
+        });
+        return ret;
     }
-}
+
+    gx.inspector = {
+        messages: function() {
+            //Return array of GX messages (msg) in form
+            return $.map($(".gx_ev"), (n,i) => { const $n = $(n); return ($n.text().length > 0) ? $n.text() : null})
+        },
+        elements: function( opts) {
+            //opts is an object with properties:
+            //ctrl_gxid is the control name as defined in Genexus (Required)
+            //row is a optional filter in case that then selected control is in a grid level (Optional)
+            //gxobjectWC is an optional filter to do the search over a specific Genexus WebComponent (Optional)
+            const {ctrl_gxid = "", row = "", gxobjectWC = ""} = opts;
+            return gx.forminspector(ctrl_gxid, row, gxobjectWC).concat(gx.inspector.grids(opts));
+        },
+        grids: function( opts) {
+            //opts is an object with properties:
+            //ctrl_gxid is the control name as defined in Genexus (Required)
+            //row is a optional filter in case that then selected control is in a grid level (Optional)
+            //Row number shold be of length 4 with '0'left padding
+            //0001001 row1 at parent row1 - 00010002 row2 at parent row1
+            //gxobjectWC is an optional filter to do the search over a specific Genexus WebComponent (Optional)
+            const {ctrl_gxid = "", row = "", gxobjectWC = ""} = opts;
+            const wcFilter = wc => !gxobjectWC || wc.ServerClass === gxobjectWC.toLowerCase() ? wc : null;
+            const gFilter = g => ((g.realGridName.toLowerCase() === ctrl_gxid.toLowerCase()) && (!row || (g.parentRow.gxId === row))) ? g : null;
+            const wcGrids = gx.O.WebComponents.filter(wcFilter).map( wc => wc.Grids).flat();
+            const mpGrids = gx.O.MasterPage ? gx.O.MasterPage.Grids : [];
+            let Grids = (!gxobjectWC ? gx.O.Grids : []).concat(wcGrids).concat(mpGrids);
+            const retObj = g =>	{
+                return {
+                    id: g.getContainerControl().id ,
+                    cmp: g.parentObject?.CmpContext || "",
+                    rows: g.grid.rows.length,
+                    inMasterPage: g.parentObject?.IsMasterPage || false,
+                }
+            };
+            return Grids.filter( gFilter).map(retObj);
+        }
+    }
+})
